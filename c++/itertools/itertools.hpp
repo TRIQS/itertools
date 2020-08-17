fe6e91419c76adcb47db4a529e0cca4ec1b6e09a
chuffa <32609153+chuffa@users.noreply.github.com>
2020-08-17 07:20:29 -0400

    Fixed unwanted behavior of ranges

--- c++/itertools/itertools.hpp
+++ c++/itertools/itertools.hpp
@@ -542,7 +542,12 @@ namespace itertools {
      * @param step__
      * The step between two indices
      */
-    range(index_t first__, index_t last__, index_t step__ = 1) : first_(first__), last_(last__), step_(step__) {}
+    range(index_t first__, index_t last__, index_t step__ = 1) : first_(first__), last_(last__), step_(step__) {
+      // make empty range e.g. range(7,3) should do nothing
+      if(first_ > last_){
+        first_ = last_;
+      }
+    }
 
     /**
      * Constructor
