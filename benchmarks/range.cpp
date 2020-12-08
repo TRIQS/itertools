67aec105931875a433f737b3dfc8f0731a8721d1
Nils Wentzell <nwentzell@flatironinstitute.org>
2020-12-08 15:06:42 -0500

    Add benchmarks for range and product_range

---
+++ benchmarks/range.cpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2019-2020 Simons Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <benchmark/benchmark.h>
+#include <itertools/itertools.hpp>
+
+using namespace itertools;
+
+// ===== Range Loop
+
+static void loop_range(benchmark::State &state) {
+  long a = 0;
+  long b = 1 << state.range(0);
+  long s = state.range(1);
+
+  for (auto _ : state){
+    for (auto i: range(a, b, s)){
+      benchmark::ClobberMemory();
+    }
+  }
+}
+BENCHMARK(loop_range)->ArgsProduct({{10, 20}, {1, 5}});
+
+// ===== Bare Loop
+
+static void loop_bare(benchmark::State &state) {
+  long a = 0;
+  long b = 1 << state.range(0);
+  long s = state.range(1);
+
+  for (auto _ : state){
+    for (auto i = a; i < b; i += s) {
+      benchmark::ClobberMemory();
+    }
+  }
+}
+BENCHMARK(loop_bare)->ArgsProduct({{10, 20}, {1, 5}});
