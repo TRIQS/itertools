4f92629a23b9bf08562add0d68e766155b17f9fe
gen_copyright <mail@server.com>
2024-05-16 08:20:54 -0400

    Regenerate Apache copyright headers

--- test/c++/itertools.cpp
+++ test/c++/itertools.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2022 Simons Foundation
+// Copyright (c) 2020-2024 Simons Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Authors: Nils Wentzell
+// Authors: Thomas Hahn, Nils Wentzell
 
 #include <gtest/gtest.h>
 #include <itertools/itertools.hpp>
